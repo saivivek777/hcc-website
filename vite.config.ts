e3c5d619051ab4aebd253937edcdb15c5e1ed81d
Sai Vivek Sai <saiviveksai716@gmail.com>
2025-11-06 10:51:08 +0530

    Fix: added base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // 👈 THIS fixes the white/blank screen issue on Vercel
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
